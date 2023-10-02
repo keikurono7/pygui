090eabe5fabec8a2f85dca4d8ee0485bfd6d4620
MadhusudanPathani <125114994+MadhusudanPathani@users.noreply.github.com>
2023-10-02 09:56:15 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+        # Process the user's input here
+        # You can store it in a database, perform calculations, etc.
+        # For demonstration purposes, we'll just display the input.
+        return f'You entered: {user_input}'
     return render_template('gui.html')
 
 if __name__ == '__main__':
